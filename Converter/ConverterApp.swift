eb5d71e697744793721035360d807dfda7703cee
Becky Richardson <rebecca.s.richardson4@gmail.com>
2023-03-13 09:19:26 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Becky Richardson on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
